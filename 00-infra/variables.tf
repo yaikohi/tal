2f8acb3ed6c234d063a1f5f46c768848c2e94762
ykhi <erik.beem@hotmail.com>
2026-02-11 13:49:06 +0100

    WIP; fix talos iso's, fix ssh key

--- 00-infra/variables.tf
+++ 00-infra/variables.tf
@@ -1,14 +1,14 @@
 variable "PROXMOX_VE_ENDPOINT" {
   description = "The endpoint for the Proxmox Virtual Environment API (example: https://host:port)"
   type        = string
-  default     = "https://192.168.10.5:8006"
+  default     = "https://192.168.10.6:8006"
   sensitive   = false
 }
 
 variable "PROXMOX_VE_NODENAME" {
   description = "Name of the proxmox node"
   type        = string
-  default     = "dusk"
+  default     = "nas"
   sensitive   = false
 }
 
